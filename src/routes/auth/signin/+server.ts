742bdc9eadf5d5e3861253e9e4516e2a68ee5874
Olle Månsson <31876997+ollema@users.noreply.github.com>
2022-08-22 09:58:47 +0200

    small changes

--- src/routes/auth/signin/+server.ts
+++ src/routes/auth/signin/+server.ts
@@ -3,13 +3,13 @@ import cookie from 'cookie';
 
 const defaultCookieOptions = { maxAge: 30 * 24 * 60 * 60, path: '/', httpOnly: true, sameSite: true, secure: true };
 
-export const POST: RequestHandler = async ({ request, locals: { pocketbase } }) => {
+export const POST: RequestHandler = async ({ request, locals }) => {
 	const response = new Response('{}');
 
 	const { email, password } = await request.json();
 	try {
-		const { token, user } = await pocketbase.users.authViaEmail(email, password);
-		pocketbase.authStore.save(token, user);
+		const { token, user } = await locals.pocketbase.users.authViaEmail(email, password);
+		locals.pocketbase.authStore.save(token, user);
 		response.headers.append('Set-Cookie', cookie.serialize('token', token, defaultCookieOptions));
 		response.headers.append('Set-Cookie', cookie.serialize('user', JSON.stringify(user), defaultCookieOptions));
 	} catch {}
